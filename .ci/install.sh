c087fff8463025496bff9961c1a6d564eab04865
taiyeba <staiyeba@gmail.com>
2019-03-27 13:37:59 +0100

    ci: adding travis ci pipeline for macos builds

---
+++ .ci/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -x
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    brew update || brew update
+    brew install cmake || true
+fi
+pip install conan --upgrade
+pip install conan_package_tools bincrafters_package_tools
+conan user
